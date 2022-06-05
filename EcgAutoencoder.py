20da5edde57c4b0abcafefcfd9526daf65884808
vrbloke <stanislawwis@gmail.com>
2022-06-05 16:39:20 +0200

    Wrap model functionality in class

---
+++ EcgAutoencoder.py
@@ -0,0 +1,65 @@
+import keras
+import keras.layers
+import numpy as np
+from keras.datasets import mnist
+
+
+class EcgAutoencoder(keras.Model):
+    """
+    Klasa daje dostęp do trzech MLP: encoder, decoder i autoencoder
+    Po konstrukcji instancji są już skompilowane, trzeba tylko włączyć
+    fitowanie na interesujących nas danych.
+    """
+    def __init__(self,
+                 input_size,
+                 layer_sizes=None,
+                 activations=None,
+                 encoder_output_index=None,
+                 loss='binary_crossentropy'):
+        """
+        :param input_size: Rozmiar danych wejściowych
+        :param layer_sizes: Lista rozmiarów warstw ukrytych (tj. nie licząc wejścia i wyjścia, które mają rozmiar input_size)
+        :param activations: Lista aktywacji warstw ukrytych (warstwa wyjściowa ma aktywację sigmoid)
+        :param encoder_output_index: Indeks warstwy, która ma być wyjściem dekodera. Jeśli nie podano, będzie równy indeksowi najmniejszej warstwy.
+        :param loss: Wiadomo
+        """
+        super().__init__()
+        if layer_sizes is None:
+            layer_sizes = [150, 70, 30, 70, 150]
+        if activations is None:
+            activations = ['relu' for _ in range(layer_sizes)]
+        if encoder_output_index is None:
+            encoder_output_index = next(i for i, v in enumerate(layer_sizes) if v == min(layer_sizes))
+        layer_sizes.append(input_size)
+        activations.append('sigmoid')
+        self.input_l = keras.Input(shape=(input_size,))
+        self.dense_layers = [keras.layers.Dense(size, activation)
+                             for size, activation in zip(layer_sizes, activations)]
+        # Build models
+        self.encoder_layers = self.dense_layers[:encoder_output_index+1]
+        self.decoder_layers = self.dense_layers[encoder_output_index+1:]
+        tensor = self.input_l
+        for i, layer in enumerate(self.dense_layers):
+            tensor = layer(tensor)
+            if i == encoder_output_index:
+                encoded_tensor = tensor
+        self.autoencoder = keras.Model(self.input_l, tensor)
+        self.encoder = keras.Model(self.input_l, encoded_tensor)
+        encoded_input = keras.Input(shape=(layer_sizes[encoder_output_index],))
+        tensor = encoded_input
+        for layer in self.decoder_layers:
+            tensor = layer(tensor)
+        self.decoder = keras.Model(encoded_input, tensor)
+        self.autoencoder.compile(optimizer='adam', loss=loss)
+
+    def get_encoder(self):
+        return self.encoder
+
+    def get_decoder(self):
+        return self.decoder
+
+    def get_autoencoder(self):
+        return self.autoencoder
+
+    def call(self, inputs, training=None, mask=None):
+        return self.decoder_layers[-1]
--- main.py
+++ main.py
@@ -1,78 +1,75 @@
-import os
+import keras
 import numpy as np
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
-import warnings
+from keras.datasets import mnist
 import matplotlib.pyplot as plt
-import cv2
-import csv
-warnings.filterwarnings('ignore')  # to suppress the warnings
-import Autoencoder
 
-for dirname, _, filenames in os.walk('/dataset'):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
-
-
-MIT_OUTCOME = {0.: 'Normal Beat',
-                   1.: 'Supraventricular premature beat',
-                   2.: 'Premature ventricular contraction',
-                   3.: 'Fusion of ventricular and normal beat',
-                   4.: 'Unclassifiable beat'}
-
-
-def autoencoding_model(X_train, X_test):
-    pass
-    # create a model
-
-
-def temp():
-    # TODO
-    pass
-
-def preprocess_data_plot(X_train, X_test):
-
-    X_test.rename(columns={187: "Class"}, inplace=True)
-    X_train.rename(columns={187: "Class"}, inplace=True)
-
-    return X_test, X_train
-
-def preprocess_data(X_train, X_test):
-
-    X_test.rename(columns={187: "Class"}, inplace=True)
-    X_train.rename(columns={187: "Class"}, inplace=True)
-    X_train.drop(columns=['Class'], inplace=True)
-    X_test.drop(columns=['Class'], inplace=True)
-    print(X_train.shape)
-    print(X_test.shape)
-    return X_test, X_train
-
-
-def plot_image(mit_train):
-
-    plt.figure(figsize=(25, 10))
-    np_count = np.linspace(0, 186, 187)
-    np_time = np.tile(np_count, (10, 1))
-    rnd = np.random.randint(0, mit_train.shape[0], size=(10,))
-
-    for i in range(np_time.shape[0]):
-        ax = plt.subplot(2, 5, i+1)
-        ax.plot(mit_train[rnd[i]])
-        # ax.set_title(MIT_OUTCOME[])
-
-    # plt.show()
-    plt.savefig("out.png")
+from EcgAutoencoder import EcgAutoencoder
 
 
 def main():
-    mit_test = pd.read_csv('./dataset/mitbih_test.csv', header=None)
-    mit_train = pd.read_csv('./dataset/mitbih_train.csv', header=None)
-    mit_test, mit_train = preprocess_data(mit_test, mit_train)
-    # plot_image(mit_train)
-    x_axis, y_axis = Autoencoder.test(mit_train, mit_test)
-    plot_image(y_axis)
+    # # Initialize model
+    input_size = 784
+    layers = [(32, 'relu'),
+              (16, 'relu'),
+              (32, 'relu')]
+    layer_sizes = [tup[0] for tup in layers]
+    activations = [tup[1] for tup in layers]
+    encoder_output_index = 1
+    loss = 'binary_crossentropy'
+    model = EcgAutoencoder(
+        input_size=input_size,
+        layer_sizes=layer_sizes,
+        activations=activations,
+        encoder_output_index=1,
+        loss=loss
+    )
+
+    # # Load data
+    (x_train, _), (x_test, _) = mnist.load_data()
+    x_train = x_train.astype('float32') / 255
+    x_test = x_test.astype('float32') / 255
+    x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+    x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+    print(x_train.shape)
+    print(x_test.shape)
+
+    # # Fit model
+    model.autoencoder.fit(x_train, x_train,
+                          epochs=12,
+                          batch_size=256,
+                          shuffle=True,
+                          validation_data=(x_test, x_test))
+
+    encoded_imgs = model.encoder.predict(x_test)
+    decoded_imgs = model.decoder.predict(encoded_imgs)
+
+    # # Visualize results
+    n = 10
+    plt.figure(figsize=(n * 3, 4))
+    for i in range(n):
+        # Display original
+        ax = plt.subplot(3, n, i + 1)
+        plt.imshow(x_test[i].reshape(28, 28))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+        # Display encoded
+        ax = plt.subplot(3, n, i + 1 + n)
+        plt.imshow(encoded_imgs[i].reshape(4, 4))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+        # Display reconstructed
+        ax = plt.subplot(3, n, i + 1 + 2 * n)
+        plt.imshow(decoded_imgs[i].reshape(28, 28))
+        plt.gray()
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+    plt.savefig('test.png')
 
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
