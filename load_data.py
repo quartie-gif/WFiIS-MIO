271d3b9d96b2a8d7d2b1b426624de20519919580
vrbloke <stanislawwis@gmail.com>
2022-06-05 17:19:24 +0200

    Implement loading csv data

--- load_data.py
+++ load_data.py
@@ -2,15 +2,11 @@ import pandas as pd
 import numpy as np
 
 
-def load_data() -> (np.ndarray, np.ndarray, np.ndarray, np.ndarray):
+def load_ecg_data() -> (np.ndarray, np.ndarray, np.ndarray, np.ndarray):
     mitbih_train = pd.read_csv('./datasets/mitbih_train.csv', header=None).to_numpy()[:, :-1]
     mitbih_test = pd.read_csv('./datasets/mitbih_test.csv', header=None).to_numpy()[:, :-1]
     ptbdb_normal = pd.read_csv('./datasets/ptbdb_normal.csv', header=None).to_numpy()[:, :-1]
     ptbdb_abnormal = pd.read_csv('./datasets/ptbdb_abnormal.csv', header=None).to_numpy()[:, :-1]
-    print(mitbih_train.shape)
-    print(mitbih_test.shape)
-    print(ptbdb_normal.shape)
-    print(ptbdb_abnormal.shape)
     return mitbih_train, mitbih_test, ptbdb_normal, ptbdb_abnormal
 
 
--- main.py
+++ main.py
@@ -1,14 +1,22 @@
+import random
+
 import keras
 import numpy as np
 from keras.datasets import mnist
 import matplotlib.pyplot as plt
 
 from EcgAutoencoder import EcgAutoencoder
+from load_data import load_ecg_data
 
 
 def main():
+    # # Load data
+    x_train, x_test, _, _ = load_ecg_data()
+    print(x_train.shape)
+    print(x_test.shape)
+
     # # Initialize model
-    input_size = 784
+    input_size = x_train.shape[1]
     layers = [(32, 'relu'),
               (16, 'relu'),
               (32, 'relu')]
@@ -24,15 +32,6 @@ def main():
         loss=loss
     )
 
-    # # Load data
-    (x_train, _), (x_test, _) = mnist.load_data()
-    x_train = x_train.astype('float32') / 255
-    x_test = x_test.astype('float32') / 255
-    x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-    x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
-    print(x_train.shape)
-    print(x_test.shape)
-
     # # Fit model
     model.autoencoder.fit(x_train, x_train,
                           epochs=12,
@@ -40,35 +39,27 @@ def main():
                           shuffle=True,
                           validation_data=(x_test, x_test))
 
-    encoded_imgs = model.encoder.predict(x_test)
-    decoded_imgs = model.decoder.predict(encoded_imgs)
+    encoded_ecg = model.encoder.predict(x_test)
+    decoded_ecg = model.decoder.predict(encoded_ecg)
 
-    # # Visualize results
-    n = 10
+    # # Visualize results]
+    n = 5
+    indices = [random.randint(0, len(x_test)) for _ in range(n)]
     plt.figure(figsize=(n * 3, 4))
-    for i in range(n):
+    for vis_i, i in enumerate(indices):
         # Display original
-        ax = plt.subplot(3, n, i + 1)
-        plt.imshow(x_test[i].reshape(28, 28))
-        plt.gray()
-        ax.get_xaxis().set_visible(False)
-        ax.get_yaxis().set_visible(False)
+        ax = plt.subplot(3, n, vis_i + 1)
+        plt.plot(range(len(x_test[i])), x_test[i])
 
         # Display encoded
-        ax = plt.subplot(3, n, i + 1 + n)
-        plt.imshow(encoded_imgs[i].reshape(4, 4))
-        plt.gray()
-        ax.get_xaxis().set_visible(False)
-        ax.get_yaxis().set_visible(False)
+        ax = plt.subplot(3, n, vis_i + 1 + n)
+        plt.plot(range(len(encoded_ecg[i])), encoded_ecg[i])
 
         # Display reconstructed
-        ax = plt.subplot(3, n, i + 1 + 2 * n)
-        plt.imshow(decoded_imgs[i].reshape(28, 28))
-        plt.gray()
-        ax.get_xaxis().set_visible(False)
-        ax.get_yaxis().set_visible(False)
+        ax = plt.subplot(3, n, vis_i + 1 + 2 * n)
+        plt.plot(range(len(decoded_ecg[i])), decoded_ecg[i])
 
-    plt.savefig('test.png')
+    plt.show()
 
 
 if __name__ == '__main__':
